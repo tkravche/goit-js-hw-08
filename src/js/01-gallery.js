59e573ac91c0fc89b46bc95c3a57e59714b6facd
Tanya <tkravche@yahoo.com>
2023-03-19 19:26:35 +0100

    Task 1 and 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightBox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+let galleryList = '';
+galleryItems.forEach(item => {
+  galleryList += `<div class="gallery__item">
+  <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></div>`;
+});
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryList);
+
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+const formEL = document.querySelector('.feedback-form');
+formEL.addEventListener('input', throttle(onFormInput, 500));
+formEL.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  let savedMessages = localStorage.getItem(LOCALSTORAGE_KEY);
+  savedMessages = savedMessages ? JSON.parse(savedMessages) : {};
+  savedMessages[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(savedMessages));
+}
+
+function populateInputs() {
+  let savedMessages = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedMessages) {
+    savedMessages = JSON.parse(savedMessages);
+    Object.entries(savedMessages).forEach(([name, value]) => {
+      savedMessages[name] = value;
+      formEL.elements[name].value = value;
+    });
+  }
+}
+populateInputs();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
